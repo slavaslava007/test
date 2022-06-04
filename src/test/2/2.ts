c38302294b6273308cd66b94059881ca5c117c1c
slavaslava007 <nicolay.lepeshkin@gmail.com>
2022-06-04 11:41:41 +0300

    first commit

---
+++ src/test/2/2.ts
@@ -0,0 +1,24 @@
+
+export  function sum(a:number,b:number) {
+    return a+b
+}
+
+export  function mult(a:number,b:number) {
+    return a*b
+}
+
+export function splitintoWord(sentese:string) {
+ const words=sentese.toLowerCase().split('')
+        return   words.filter(w=> w !=="")
+    .map(w=>w.replace('!',''))
+}
+
+
+
+
+// const sentense = 'Hello my friends!'
+//
+// function SplitIntoWord(sentense: string) {
+//     return ['hello', 'my', 'friends']
+// }
+
---
+++ src/test/3/3.ts
@@ -0,0 +1,57 @@
+type Techtype={
+    id:number
+    title:string
+}
+type CityType={
+    title: string
+    country: string
+}
+type AdressType={
+    street:string
+    city:CityType
+}
+type StudentType={
+    id:number
+    name:string
+    age:number
+    isActive:boolean
+    adress:AdressType
+    tech:Array<Techtype>
+}
+
+const itincubator = {
+    id:1,
+    name: 'it-incubator',
+    age: 1060,
+    isActive: true,
+    adress: {
+        street: 'platonova',
+        city: {
+            country: 'Belarus',
+            title: 'Minsk'
+        }
+    },
+
+    tech:[
+        {id:1,
+            title:'html'
+        },
+        {id:2,
+            title:'JS'
+        },
+        {id:3,
+            title:'React'
+        },
+    ]
+}
+
+
+console.log(itincubator.age)
+console.log(itincubator.isActive)
+console.log(itincubator.adress.city)
+console.log(itincubator.tech[1].title)
+
+
+
+
+
